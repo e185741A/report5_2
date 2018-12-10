7b7909fc8ece63c7299d140bc78b62f4e615df4b
Atsuto Goya <e185741@ie.u-ryukyu.ac.jp>
2018-12-10 20:28:35 +0900

    三度目の正直　part1

---
+++ src/main/java/jp/ac/uryukyu/ie/e185741/Enemy.java
@@ -0,0 +1,41 @@
+package jp.ac.uryukyu.ie.e185741;
+
+public class Enemy extends LivingThing {
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     *
+     * @param name      モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack    モンスターの攻撃力
+     */
+    public Enemy(String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    /**
+     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
+     *
+     * @return boolean
+     */
+
+    /**
+     * Heroへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     *
+     * @param hero 攻撃対象
+     */
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage) {
+        setHitpoint(getHitPoint() - damage);
+        if (getHitPoint() < 0) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+}
+
